babe8f080af0472a1842254628e5881eda0ded4f
mtopolski1 <mtopolski1@gmail.com>
2021-07-12 21:43:46 -0400

    initial commit

---
+++ MidtermLibraryTerminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MidtermLibraryTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Midterm Project! Library Terminal. Team members Kate, Morgan and Mark");
+        }
+    }
+}
