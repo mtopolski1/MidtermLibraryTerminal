7f0182f5d38399cd48c49fe342e86efaa8eaeb4a
Morgan Bateman <mbateman855@gmail.com>
2021-07-14 18:40:49 -0400

    Added menu functionality and some test objects

---
+++ MidtermLibraryTerminal/Menu.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidtermLibraryTerminal
+{
+    class Menu
+    {
+        public static void MenuStart(List<Option> options, string prompt)
+        {
+            int index = 0;
+
+            WriteMenu(options, options[index], prompt);
+
+            bool menuLoop = true;
+
+            //used like FileInfo;
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    if(index + 1 < options.Count) //if there is an option below the current option, allow the down arrow input
+                    {
+                        index++; //set the index to the next option BEFORE calling the WriteMenu. otherwise it will just write out the same menu
+                        WriteMenu(options, options[index], prompt);
+                    }
+                }
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (index - 1 >= 0) //if there is an option above the current option
+                    {
+                        index--;
+                        WriteMenu(options, options[index], prompt);
+                    }
+                }
+
+                if(keyInfo.Key == ConsoleKey.Enter)
+                {
+                    options[index].Selected.Invoke(); //do the Action associated with the option
+                    index = 0; //reset the index back to the beginning
+                    menuLoop = false; //break out of the menu's loop
+                }
+            }
+            while (menuLoop == true);
+        }
+
+        public static void WriteMenu(List<Option> options, Option selectedOption, string prompt)
+        {
+            Console.Clear();
+            Console.WriteLine($"{prompt}\n");
+
+            foreach(Option option in options)
+            {
+                if (option == selectedOption)
+                {
+                    //way of distinguishing which option is the current option.
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(string.Empty);
+                }
+
+                Console.WriteLine(option.OptionText);
+
+                Console.ResetColor();
+            }
+        }
+    }
+}
--- MidtermLibraryTerminal/Program.cs
+++ MidtermLibraryTerminal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MidtermLibraryTerminal
 {
@@ -7,6 +9,83 @@ namespace MidtermLibraryTerminal
         static void Main(string[] args)
         {
             Console.WriteLine("This is the Midterm Project! Library Terminal. Team members Kate, Morgan and Mark");
+
+
+            //the Action for the option MUST be a method that does not have a parameter. 
+            //unfortunately that means we will have to access the file with all of the books in it in each and every method
+            Option testOne = new Option("View entire book list", ListBooks);
+            Option testTwo = new Option("Search by Title", TitleSearch);
+            Option testThree = new Option("Search by Author", AuthorSearch);
+            Option testFour = new Option("Return a book", BookReturn);
+            Option testFive = new Option("Quit", SaveAndQuit);
+
+
+            List<Option> testList = new List<Option> { testOne, testTwo, testThree, testFour, testFive };
+            Menu.MenuStart(testList, "Welcome to the Library Terminal. Would you like to:\n");
         }
+        static void ListBooks()
+        {
+            Console.Clear();
+
+            Book bookOne = new Book
+            {
+                Title = "Test book one",
+                Author = "Test author one",
+                Status = true,
+                DueDate = DateTime.Now
+            };
+            Book bookTwo = new Book
+            {
+                Title = "Test book two",
+                Author = "Test author two",
+                Status = true,
+                DueDate = DateTime.Now
+            };
+            Book bookThree = new Book
+            {
+                Title = "Test book three",
+                Author = "Test author three",
+                Status = false,
+                DueDate = DateTime.Today
+            };
+            Book bookFour = new Book
+            {
+                Title = "Test book four",
+                Author = "Test author four",
+                Status = true,
+                DueDate = DateTime.Now
+            };
+            Book bookFive = new Book
+            {
+                Title = "Test book five",
+                Author = "Test author five",
+                Status = true,
+                DueDate = DateTime.Now
+            };
+
+            List<Book> bookList = new List<Book> { bookOne, bookTwo, bookThree, bookFour, bookFive };
+
+            foreach (Book book in bookList)
+            {
+                Console.WriteLine($"{book.Title} - {book.Author}");
+            }
+        }
+        static void TitleSearch()
+        {
+
+        }
+        static void AuthorSearch()
+        {
+
+        }
+        static void BookReturn()
+        {
+
+        }
+        static void SaveAndQuit()
+        {
+
+        }
+        
     }
 }
