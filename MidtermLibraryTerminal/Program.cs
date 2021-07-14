407d53598635fb7e99f71711a6219f68ca1fa03b
Morgan Bateman <mbateman855@gmail.com>
2021-07-14 19:55:38 -0400

    Commented out test code so as not to interfere with final code

--- MidtermLibraryTerminal/Program.cs
+++ MidtermLibraryTerminal/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace MidtermLibraryTerminal
 {
@@ -10,22 +11,25 @@ namespace MidtermLibraryTerminal
         {
             Console.WriteLine("This is the Midterm Project! Library Terminal. Team members Kate, Morgan and Mark");
 
+            #region Menu Testing
+            ////the Action for the option MUST be a method that does not have a parameter. 
+            ////unfortunately that means we will have to access the file with all of the books in it in each and every method
+            //Option testOne = new Option("View entire book list", ListBooks);
+            //Option testTwo = new Option("Search by Title", TitleSearch);
+            //Option testThree = new Option("Search by Author", AuthorSearch);
+            //Option testFour = new Option("Return a book", BookReturn);
+            //Option testFive = new Option("Quit", SaveAndQuit);
 
-            //the Action for the option MUST be a method that does not have a parameter. 
-            //unfortunately that means we will have to access the file with all of the books in it in each and every method
-            Option testOne = new Option("View entire book list", ListBooks);
-            Option testTwo = new Option("Search by Title", TitleSearch);
-            Option testThree = new Option("Search by Author", AuthorSearch);
-            Option testFour = new Option("Return a book", BookReturn);
-            Option testFive = new Option("Quit", SaveAndQuit);
 
-
-            List<Option> testList = new List<Option> { testOne, testTwo, testThree, testFour, testFive };
-            Menu.MenuStart(testList, "Welcome to the Library Terminal. Would you like to:\n");
+            //List<Option> testList = new List<Option> { testOne, testTwo, testThree, testFour, testFive };
+            //Menu.MenuStart(testList, "Welcome to the Library Terminal. Would you like to:\n");
+            #endregion
         }
         static void ListBooks()
         {
             Console.Clear();
+
+            //make the book list be scrollable
 
             Book bookOne = new Book
             {
@@ -69,9 +73,12 @@ namespace MidtermLibraryTerminal
             {
                 Console.WriteLine($"{book.Title} - {book.Author}");
             }
+            //back to the previous menu
         }
+
         static void TitleSearch()
         {
+            //BookInOut();
 
         }
         static void AuthorSearch()
